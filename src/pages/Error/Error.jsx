06ad2a465c54ecc3f7fdf8aa25bb8650ba166d24
Vangelis-26 <mourier.matthieu@gmail.com>
2025-05-06 17:31:29 +0200

    Add Error Page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,3 +1,14 @@
-export function Error404() {
-    return <h1>Erreur 404 - Page non trouvée</h1>;
+import { Link } from "react-router-dom";
+import styles from './Error.module.scss';
+
+export function Error() {
+    return (
+        <div className={styles.error}>
+            <div className={styles.error_404}>404</div>
+            <div className={styles.error_message}>Oups! La page que vous demandez n'existe pas.</div>
+            <Link to="/" className={styles.error_homeLink}>
+                Retourner sur la page d’accueil
+            </Link>
+        </div>
+    );
 }
