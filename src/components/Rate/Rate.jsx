b7c727598b220ae2b57c793fca1a2eced8b8a598
Vangelis-26 <mourier.matthieu@gmail.com>
2025-05-08 01:19:25 +0200

    Add Rate Component

---
+++ src/components/Rate/Rate.jsx
@@ -0,0 +1,23 @@
+
+import StarFull from '../../assets/rates/StarFull.svg';
+import StarEmpty from '../../assets/rates/starEmpty.svg';
+import styles from './Rate.module.scss';
+
+export function Rate({ rating }) {
+
+    const totalStars = 5;
+    const stars = []
+
+    for (let i = 1; i <= totalStars; i++) {
+        if (i <= rating) {
+            stars.push(<img key={i} src={StarFull} alt="star" className={styles.star} />);
+        } else {
+            stars.push(<img key={i} src={StarEmpty} alt="star" className={styles.star} />);
+        }
+    }
+    return (
+        <div className={styles.rate}>
+            {stars}
+        </div>
+    );
+}
